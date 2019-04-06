e55cdec3b1c43fbe106927ebe998620937513b9a
mike <redmike@mindspring.com>
2019-04-05 21:04:46 -0400

    Loader work.  Incremental development.

--- app/src/main/java/com/example/android/newsfeed/ArticleLoader.java
+++ app/src/main/java/com/example/android/newsfeed/ArticleLoader.java
@@ -1,10 +1,15 @@
 package com.example.android.newsfeed;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v4.content.AsyncTaskLoader;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+
+
 
 public class ArticleLoader extends AsyncTaskLoader<List<ArticleData>> {
 
@@ -14,6 +19,23 @@ public class ArticleLoader extends AsyncTaskLoader<List<ArticleData>> {
 
     @Override
     public List<ArticleData> loadInBackground() {
+
+        try {
+        List<ArticleData> articleList = null;
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.scheme(getContext().getString(R.string.http_string))
+            .encodedAuthority("content.guardianapis.com")
+            .appendPath("search")
+            .appendQueryParameter("order-by", "newest");
+
+        String jsonString = uriBuilder.toString();
+        
+
+       // } catch (IOException e) {
+       //     ;
+       // }
+
+
         return null;
     }
 }
