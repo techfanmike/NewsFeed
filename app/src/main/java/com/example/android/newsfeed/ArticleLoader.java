4ac1851660e53ee52d11a563019b6091ec060024
mike <redmike@mindspring.com>
2019-04-10 23:35:43 -0400

    Restructured for Query Utils class developmental build 3.

--- app/src/main/java/com/example/android/newsfeed/ArticleLoader.java
+++ app/src/main/java/com/example/android/newsfeed/ArticleLoader.java
@@ -1,32 +1,29 @@
 package com.example.android.newsfeed;
 
+import android.content.AsyncTaskLoader;
 import android.content.Context;
-import android.net.Uri;
-import android.support.v4.content.AsyncTaskLoader;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.List;
-
-
 
 public class ArticleLoader extends AsyncTaskLoader<List<ArticleData>> {
 
-    public ArticleLoader(Context context) {
+    private String mUrl;
+
+    public ArticleLoader(Context context, String url) {
         super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
     }
 
     @Override
     public List<ArticleData> loadInBackground() {
-        return null;
+        if (mUrl == null) {return null;}
+
+        List<ArticleData> articles = QueryUtils.fetchArticleData(mUrl);
+        return articles;
     }
 }
--- app/src/main/java/com/example/android/newsfeed/MainActivity.java
+++ app/src/main/java/com/example/android/newsfeed/MainActivity.java
@@ -1,42 +1,73 @@
 package com.example.android.newsfeed;
 
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<ArticleData>> {
+public class MainActivity extends AppCompatActivity
+        implements LoaderCallbacks<List<ArticleData>> {
+
+    private ArticleListAdapter articleListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: complete layout
+        articleListAdapter = new ArticleListAdapter(this, 5, new ArrayList<ArticleData>());
+
+        // TODO: complete layout
+        ListView articleList = findViewById(5);
+        articleList.setAdapter(articleListAdapter);
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            LoaderManager loaderManager = getLoaderManager();
+        }
     }
 
-    private ArticleListAdapter articleListAdapter;
 
     @NonNull
     @Override
     public Loader<List<ArticleData>> onCreateLoader(int i, @Nullable Bundle bundle) {
-        return new ArticleLoader(this);
+        Uri baseUri = Uri.parse("https://content.guardianapis.com/search");
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("section", "news");
+        uriBuilder.appendQueryParameter("page_size", "150");
+        uriBuilder.appendQueryParameter("show_tags", "contributor");
+        uriBuilder.appendQueryParameter("from_date", "2019-03-01");
+        uriBuilder.appendQueryParameter("to-date", "2019-04-01");
+        uriBuilder.appendQueryParameter("api-key", getString(R.string.guardian_key));
+
+        return new ArticleLoader(this, uriBuilder.toString());
     }
 
     @Override
     public void onLoadFinished(@NonNull Loader<List<ArticleData>> loader, List<ArticleData> articleData) {
-        if (articleListAdapter != null) {
-            articleListAdapter.setNotifyOnChange(false);
-            articleListAdapter.clear();
-            articleListAdapter.setNotifyOnChange(true);
-            articleListAdapter.addAll(articleData);
-        }
+        ;
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader<List<ArticleData>> loader) {
+        articleListAdapter.clear();
     }
 
 }
