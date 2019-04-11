df8c6acdb6216d7eaef9c48dd806048cadb62400
mike <redmike@mindspring.com>
2019-04-10 22:23:30 -0400

    Restructured for Query Utils class.

---
+++ app/src/main/java/com/example/android/newsfeed/QueryUtils.java
@@ -0,0 +1,124 @@
+package com.example.android.newsfeed;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class QueryUtils {
+
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
+
+    // private constructor, should not be called
+    private QueryUtils() {;}
+
+    public static List<ArticleData> fetchArticleData(String requestUrl) {
+
+        URL url = createUrl(requestUrl);
+
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        // return the list of articles
+        return extractFeatureFromJson(jsonResponse);
+    }
+
+    // return a URL object from a string
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        if(url == null){return jsonResponse;}
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            if(urlConnection.getResponseCode()  == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            }
+
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+        } finally {
+            if(urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if(inputStream != null) {
+                inputStream.close();
+            }
+        }
+
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    private static List<ArticleData> extractFeatureFromJson(String articleJson) {
+
+        if(TextUtils.isEmpty(articleJson)) { return null;}
+
+        List<ArticleData> articles = new ArrayList<>();
+
+        try {
+
+            JSONObject baseJsonResponse = new JSONObject(articleJson);
+            JSONArray articleArray = baseJsonResponse.getJSONArray("features");
+
+            for (int index1 = 0; index1 < articleArray.length(); index1++) {
+                JSONObject currentArticle = articleArray.getJSONObject(index1);
+            }
+
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem paring the article JSON results", e);
+        }
+
+        return articles;
+    }
+}
